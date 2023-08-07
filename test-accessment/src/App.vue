24acb0c50758c2d1f878a387e1963dcbc9fa2ab6
abubakarsd <abubakarsd.harunasadiq@gmail.com>
2023-08-07 14:36:10 +0100

    first commit

---
+++ test-accessment/src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import task from './components/task.vue'
+</script>
+
+<template>
+  <!-- <header>
+
+    <div class="wrapper">
+      <task msg="You did it!" />
+    </div>
+  </header> -->
+
+  <main>
+    <task />
+  </main>
+</template>
+
+<style>
+.todo-table {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid #ccc;
+  margin-top: 20px;
+}
+
+.todo-table th,
+.todo-table td {
+  border: 1px solid #ccc;
+  padding: 8px;
+  text-align: center;
+}
+
+.todo-table th {
+  background-color: #22e279;
+  font-weight: bold;
+}
+.edit-button,
+.update-button {
+  border: none;
+  padding: 6px 12px;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 14px;
+  transition: background-color 0.3s ease;
+  margin: 5px;
+}
+
+.edit-button {
+  background-color: #3498db;
+  color: #fff;
+}
+
+.edit-button:hover {
+  background-color: #2980b9;
+}
+
+.update-button {
+  background-color: #27ae60;
+  color: #fff;
+}
+
+.update-button:hover {
+  background-color: #219653;
+}
+</style>
---
+++ test-accessment/src/components/task.vue
@@ -0,0 +1,49 @@
+<template>
+<div id="app">
+        <table class="todo-table">
+            <thead>
+                <tr>
+                    <th>Todo Name</th>
+                    <th>Progress</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(todo, index) in todos" :key="index">
+                    <td>{{ todo.name }}</td>
+                    <td>{{ todo.progress }}% Complete</td>
+                    <td>
+                        <button @click="editTodo(index)" class="edit-button">Edit</button>
+                        <button @click="updateProgress(index)" class="update-button">Update</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      todos: [
+        { name: "First Accessment", progress: 25 },
+        { name: "Second Accessment", progress: 50 },
+        { name: "Third Accessment", progress: 75 },
+      ],
+    };
+  },
+  methods: {
+    editTodo(index) {
+      // Implement edit functionality here
+      console.log("Edit clicked for index:", index);
+    },
+    updateProgress(index) {
+      // Implement update functionality here
+      console.log("Update clicked for index:", index);
+    },
+  },
+};
+</script>
